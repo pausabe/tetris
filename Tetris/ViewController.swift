a914374f5dd176d512f65f1817640f149bc4a08b
Pau Sabé <pau.sabe@icloud.com>
2022-08-31 16:42:25 +0200

    Initial Commit

---
+++ Tetris/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tetris
+//
+//  Created by Pau Sabé Martínez on 31/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

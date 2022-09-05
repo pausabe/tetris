5a5e23adc3c2e8afc6c6064d622641ea6c21d640
Pau Sabé <pau.sabe@icloud.com>
2022-09-05 17:15:48 +0200

    Preparing Tetrominio model for future implementation

---
+++ Tetris/Model/Tetrominio.swift
@@ -0,0 +1,13 @@
+//
+//  Tetrominio.swift
+//  Tetris
+//
+//  Created by Pau Sabé Martínez on 5/9/22.
+//
+
+import Foundation
+
+public struct Tetrominio{
+    var currentPositionX = 0
+    var currentPositionY = 0
+}

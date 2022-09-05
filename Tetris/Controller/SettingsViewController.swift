c41a03d517f43d53f21b89c77d845a0590845c3b
Pau Sabé <pau.sabe@icloud.com>
2022-09-05 17:18:04 +0200

    Added new Settings view controller

---
+++ Tetris/Controller/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  Tetris
+//
+//  Created by Pau Sabé Martínez on 5/9/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func audioSwitchValueChanged(_ sender: UISwitch) {
+        // TODO: implement something like store service
+    }
+}
